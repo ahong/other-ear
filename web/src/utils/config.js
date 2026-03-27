021b879a4c217b1d9d445c51ac84b866f892cffe
ahong <1002363357@qq.com>
2026-03-28 00:55:09 +0800

    Revert "del"
    This reverts commit a92872256544a55db491927fc9d0cbbcbfe2ac9a.

---
+++ web/src/utils/config.js
@@ -0,0 +1,4 @@
+// 腾讯云 API 密钥
+// 请前往 https://console.cloud.tencent.com/cam/capi 获取并填入
+export const TENCENT_SECRET_ID = 'AKIDVEzBd7bv41nXGzpvwssnmeP1TlTFzojV'
+export const TENCENT_SECRET_KEY = 'e4JXdWGsKcjgSClBTccVHuPn04zhEcpt '
